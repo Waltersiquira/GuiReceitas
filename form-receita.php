b363a278fb793bbb583a983cc013cccb14d31ba7
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-07 11:57:39 -0300

    Create form-receita.php

---
+++ form-receita.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiReceitas</title>
+</head>
+<body>
+    <form action="enviar-form-receita.php" method="get">
+        digite o nome da receita <br>
+        <input type="text" name="nome"> <br>
+        digite a descrição da receita <br>
+        <input type="text" name="descricao"> <br>
+        digite a imagem da receita <br>
+        <input type="text" name="imagem"> <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
