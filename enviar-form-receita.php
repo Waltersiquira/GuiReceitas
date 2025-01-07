9b08a4b02eb9e896219daceda3d67e7b3f700b28
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-07 12:05:24 -0300

    Create enviar-form-receita.php

---
+++ enviar-form-receita.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/favicon.ico" type="image/x-icon">
+    <title>GuiReceitas</title>
+</head>
+<body>
+    <?php 
+    require_once 'includes/sistema.php';
+    $nome = $_GET['nome'] ?? '';
+    $descricao = $_GET['descricao'] ?? '';
+    $imagem = $_GET['imagem'] ?? '';
+    if (!empty($nome) and !empty($descricao) and !empty($imagem)){
+        if ($sistem->query("INSERT INTO `receitas` (`id`, `nome`, `descrição`, `imagem`) VALUES (DEFAULT, '$nome', '$descricao', '$imagem')") == true){
+            echo "Receita adcionada com sucesso";
+        } else {
+            echo 'erro ao inserir dados';
+        }
+    } else {
+        echo "erro não pode ter nome, descricao, imagem vazia";
+    }
+    ?>
+</body>
+</html>
