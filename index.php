d198667d898eef0e6df2787f3f1062be5fad09ae
Waltersiquira <167370881+Waltersiquira@users.noreply.github.com>
2025-01-07 11:33:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GuiReceitas</title>
+</head>
+<body>
+    <button><a href="form-receita.php" style="text-decoration: none;">adcionar receita</a></button>
+    <?php require_once 'includes/sistema.php' ?>
+    <h1 style="color: brown;">GuiReceitas</h1>
+    <hr>
+    <table>
+        <?php 
+        $busca = $sistem->query('select * from receitas');
+        if (!$busca){
+            echo 'error';
+        } else {
+            if ($busca->num_rows == 0){
+                echo 'error';
+            } else {
+                while ($reg=$busca->fetch_object()){
+                    echo "<tr><td><a href='pagina-receita.php?i=$reg->id'><img src='$reg->imagem' width='200'></a></td><td>$reg->nome</td></tr>";
+                }
+            }
+        }
+        ?>
+    </table>
+    <?php $sistem->close() ?>
+</body>
+</html>
